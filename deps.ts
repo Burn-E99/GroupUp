e7568745ca1998685756005fffc9eeda862c6ce0
Ean Milligan (Bastion) <ean.milligan@gmail.com>
2023-04-29 05:02:44 -0400

    bump mysql version

--- deps.ts
+++ deps.ts
@@ -36,6 +36,6 @@ export type {
 	Interaction,
 } from 'https://deno.land/x/discordeno@12.0.1/mod.ts';
 
-export { Client } from 'https://deno.land/x/mysql@v2.10.1/mod.ts';
+export { Client } from 'https://deno.land/x/mysql@v2.11.0/mod.ts';
 
 export { initLog, log, LogTypes as LT } from 'https://raw.githubusercontent.com/Burn-E99/Log4Deno/V1.1.0/mod.ts';
