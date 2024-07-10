15d77efa1b6f60e50607bc95cce098fb144fcb8c
Ean Milligan <ean.milligan@gmail.com>
2024-07-10 00:52:38 -0400

    V1.1.6 - properly fix the formatting issue

--- src/buttons/eventUtils.ts
+++ src/buttons/eventUtils.ts
@@ -27,7 +27,7 @@ export const alternateEventBtnStr = 'Join as Alternate';
 export const noDescProvided = 'No description provided.';
 
 // Member List generators
-const escapeMemberNameForDisplay = (memberName: string): string => memberName.replace(/[_*]/g, '\\$&');
+const escapeMemberNameForDisplay = (memberName: string): string => memberName.replaceAll('\\', '').replaceAll('_', '\\_');
 export const generateMemberTitle = (memberList: Array<LFGMember>, maxMembers: number): string => `Members Joined: ${memberList.length}/${maxMembers}`;
 export const generateMemberList = (memberList: Array<LFGMember>): string =>
 	memberList.length ? memberList.map((member) => `${escapeMemberNameForDisplay(member.name)} - <@${member.id}>`).join('\n') : noMembersStr;
